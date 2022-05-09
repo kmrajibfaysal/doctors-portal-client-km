a0e1c8a22ef46ed7760c099363251a1c6ee9f01e
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-05-10 01:26:50 +0600

    finished styling nav bar

--- src/Shared/Nav.js
+++ src/Shared/Nav.js
@@ -5,54 +5,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => (
-    <div className="navbar bg-base-100">
-        <div className="navbar-start">
-            <Link to="/" className="btn btn-ghost text-xl normal-case">
-                daisyUI
-            </Link>
-        </div>
-        <div className="navbar-end ">
-            <ul className="menu menu-horizontal hidden p-0 lg:flex">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/appointment">Appointment</Link>
-                </li>
-                <li>
-                    <Link to="/reviews">Reviews</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact Us</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-            <div className="dropdown dropdown-end">
-                <label tabIndex="0" className="btn btn-ghost lg:hidden">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16"
-                        />
-                    </svg>
-                </label>
-                <ul
-                    tabIndex="0"
-                    className="dropdown-content  menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-                >
+    <nav className="container mx-auto">
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <Link to="/" className="logo text-xl font-bold md:text-2xl ">
+                    Doctors Portal
+                </Link>
+            </div>
+            <div className="navbar-end ">
+                <ul className="nav-link  hidden p-0 lg:flex">
                     <li>
                         <Link to="/">Home</Link>
                     </li>
@@ -66,15 +27,56 @@ const Nav = () => (
                         <Link to="/reviews">Reviews</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact Us</Link>
+                        <Link to="/contact">Contact</Link>
                     </li>
                     <li>
                         <Link to="/login">Login</Link>
                     </li>
                 </ul>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex="0"
+                        className="nav-link dropdown-content rounded-box  mt-3 w-52  p-2 shadow"
+                    >
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/appointment">Appointment</Link>
+                        </li>
+                        <li>
+                            <Link to="/reviews">Reviews</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
+    </nav>
 );
 
 export default Nav;
