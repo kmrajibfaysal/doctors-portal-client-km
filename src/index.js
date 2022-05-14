86b4e766fc7c72b301888eb5da83b1f802fa7849
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-05-14 07:01:53 +0600

    added react query dependecies and set up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>
 );
